250f47d29aa6bcf0bd3bb7af153813883f088b67
Datt <datbrahm@amazon.com>
2020-03-23 03:39:20 +0530

    completed the blog and portfolio in django

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,12 @@ class Blog(models.Model):
     pub_date = models.DateTimeField(default=timezone.now)
     bbody = models.TextField(max_length=10000)
     image = models.ImageField(upload_to='images/')
+
+    def summary(self):
+        return self.bbody[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+    def __str__(self):
+        return self.title
